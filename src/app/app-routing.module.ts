5a29037c952b00f425aabdf7d93fd697d5682ac7
zoemarcial2001 <zoemarcial2001@gmail.com>
2025-01-16 15:08:33 -0300

    Creacion Primeros componentes head, inventario, agregar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { AgregarComponent } from './inventario/agregar/agregar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'principal', component: PrincipalComponent},
+  {path: 'inventario', component: InventarioComponent},
+  {path: 'agregar', component: AgregarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { HeadComponent } from './head/head.component';
+import { LoginComponent } from './login/login.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { AgregarComponent } from './inventario/agregar/agregar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PrincipalComponent,
+    HeadComponent,
+    LoginComponent,
+    InventarioComponent,
+    AgregarComponent
   ],
   imports: [
     BrowserModule,
